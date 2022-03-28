f623e4020e1b4c5479779b66e54273a0b2b241b7
sveinnoli <sveinnoli@hotmail.com>
2022-03-28 23:48:43 +0000

    javascript_info docs

--- docs/javascript_info/DOM/script.js
+++ docs/javascript_info/DOM/script.js
@@ -1,5 +1,21 @@
-document.body.style.backgroundColor = 'red';
+document.body.style.backgroundColor = 'grey';
 
 setTimeout(() => {
-    document.body.style.backgroundColor = "";
+    document.body.style.backgroundColor = "darkgrey";
 }, 3000);
+
+// Access <div> & <ul> & <li>
+let div = document.getElementsByTagName("div");
+let ul = document.getElementsByTagName("ul");
+let li = document.getElementsByTagName("li");
+console.log(div, ul, li);
+
+// Method 2
+let div2 = document.body.firstElementChild;
+let ul2 = div2.nextElementSibling;
+let li2 = ul2.childNodes[3];
+let li3 = ul2.children[0];
+console.log(div2, ul2, li2, li3);
+
+console.log(document.getElementById("tst").lastChild.nextSibling); // Always returns null
+console.log(document.body.children[0].previousSibling); 
--- docs/javascript_info/readme.md
+++ docs/javascript_info/readme.md
@@ -1,3 +1,3 @@
 ## Javascript documentation by javascript.info
 [Source](https://javascript.info/document)
-[Currently at](https://javascript.info/dom-navigation)
+[Currently at](https://javascript.info/basic-dom-node-properties)
